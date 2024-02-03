f3d8ac63a0a0aa01d5bdec0cee7c57ca6e208041
paanastasiadis <panag1234@outlook.com>
2024-02-03 14:43:29 +0200

    Add * for allowed hosts

--- device_management/device_management/settings.py
+++ device_management/device_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@sqrcm3k9g66$*a&_3yzdm07$2vbh%2*n6tkaj&y0(yj4#_-0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
