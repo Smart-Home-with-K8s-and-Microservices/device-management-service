750b3a8b9d6574f1d3eec6bd64f046b483511d4a
paanastasiadis <panag1234@outlook.com>
2024-02-06 02:17:41 +0200

    Add serializer validating requested sensor command

--- device_management/devices/serializers.py
+++ device_management/devices/serializers.py
@@ -34,3 +34,47 @@ class FlashSerialDeviceSerializer(serializers.Serializer):
     class Meta:
         model = Device
         fields = '__all__'
+
+
+class CommandSerializer(serializers.Serializer):
+    sensor_id = serializers.IntegerField()
+    command = serializers.CharField()
+
+    def validate(self, data):
+        data = super().validate(data)
+
+        # Extract sensor ID and command
+        sensor_id = data.get('sensor_id', None)
+        command = data.get('command', None)
+
+        # Check if the sensor with the given ID exists
+        sensor = Sensor.objects.filter(pk=sensor_id).first()
+
+        if not sensor:
+            raise serializers.ValidationError(
+                "Sensor with this ID does not exist.")
+
+        # Retrieve the device associated with the sensor
+        device = Device.objects.filter(pk=sensor.device_id).first()
+
+        if not sensor.accepts_commands:
+            raise serializers.ValidationError(
+                "This sensor does not accept commands.")
+
+        if device.status == 'disconnected':
+            raise serializers.ValidationError(
+                "Sending command to a disconnected device.")
+
+        if device.status == 'not_set_up':
+            raise serializers.ValidationError(
+                "Sending command to a device that is not set up.")
+
+        if sensor.available_commands is not None and command not in sensor.available_commands:
+            raise serializers.ValidationError(
+                "Invalid command for this sensor.")
+
+        # Attach the sensor and device objects to the serializer data
+        data['sensor'] = sensor
+        data['device'] = device
+
+        return data
