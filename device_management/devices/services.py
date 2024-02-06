2ce209236fe5dd6c0b9ad6089c02c48271921eb1
paanastasiadis <panag1234@outlook.com>
2024-02-06 02:12:42 +0200

    Add service method that sends commands to sensors

--- device_management/devices/services.py
+++ device_management/devices/services.py
@@ -1,3 +1,8 @@
+import devices.mqtt.utils as mqtt_utils
+from devices.mqtt.service import MQTTService
+from django.conf import settings
+
+
 def update_device_status(device_serial, device_status):
     '''Update the status of a device in the database.'''
 
@@ -19,3 +24,17 @@ def update_device_status(device_serial, device_status):
     # Save the changes
     device.save()
 
+
+def send_command_to_sensor(device_serial, sensor_serial, command):
+    '''Send a command to a sensor via MQTT.'''
+
+    # get or create the mqtt service
+    mqtt_service = MQTTService(
+        broker_address=settings.MQTT_BROKER,
+        port=int(settings.MQTT_PORT))
+
+    # construct command topic
+    topic = mqtt_utils.get_command_topic(device_serial, sensor_serial)
+
+    # send command
+    mqtt_service.publish(topic, command)
